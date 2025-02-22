c4c48ec5a77e53795615df202b272ed308bec0a8
jk6841 <jk6841@gmail.com>
2025-02-22 11:01:22 +0900

    blog 제거

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Box from "@mui/material/Box";
 import { Container, Tab, Tabs } from "@mui/material";
 import { useState } from "react";
 import AboutMe from "./pages/aboutme";
-import Blog from "./pages/blog";
 
 function App() {
     const [currentTab, setCurrentTab] = useState(0);
@@ -20,11 +19,9 @@ function App() {
                     sx={{ mb: 4 }}
                 >
                     <Tab label="About Me" />
-                    <Tab label="Blog" />
                 </Tabs>
 
                 {currentTab === 0 && <AboutMe />}
-                {currentTab === 1 && <Blog />}
             </Container>
         </Box>
     );
