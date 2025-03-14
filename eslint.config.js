d88708e90b571dd47f2d2137cc405f65ff6baaf7
jk6841 <jk6841@gmail.com>
2025-03-14 20:12:12 +0900

    eslint 적용

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['docs', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,8 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'error',
     },
   },
 ];
--- src/pages/aboutme/sections/WorkExperience.jsx
+++ src/pages/aboutme/sections/WorkExperience.jsx
@@ -1,7 +1,6 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { Stack } from "@mui/material";
-import { useMemo } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Stack } from '@mui/material';
 import Java from '../../../components/skills/Java';
 import SpringBoot from '../../../components/skills/SpringBoot';
 import MySQL from '../../../components/skills/MySQL';
@@ -10,67 +9,71 @@ import Kafka from '../../../components/skills/Kafka';
 import Docker from '../../../components/skills/Docker';
 import Kubernetes from '../../../components/skills/Kubernetes';
 import TypeScript from '../../../components/skills/TypeScript';
-import Go from "../../../components/skills/Go";
-import Jenkins from "../../../components/skills/Jenkins";
-import Gradle from "../../../components/skills/Gradle";
-import Airflow from "../../../components/skills/Airflow";
-import Vue2 from "../../../components/skills/Vue2";
-import React from "../../../components/skills/React";
-import Kotlin from "../../../components/skills/Kotlin";
-import GithubActions from "../../../components/skills/GithubActions";
-import WorkExperienceItem from "./WorkExperienceItem";
+import Go from '../../../components/skills/Go';
+import Jenkins from '../../../components/skills/Jenkins';
+import Gradle from '../../../components/skills/Gradle';
+import Airflow from '../../../components/skills/Airflow';
+import Vue2 from '../../../components/skills/Vue2';
+import React from '../../../components/skills/React';
+import Kotlin from '../../../components/skills/Kotlin';
+import GithubActions from '../../../components/skills/GithubActions';
+import WorkExperienceItem from './WorkExperienceItem';
 
 const now = new Date();
 
 const baseData = [
-    {
-        company: "NHN Cloud",
-        startDate: new Date('2022-01-03'),
-        endDate: new Date('2025-02-04'),     
-        skills: [
-            [Java, TypeScript, Go],
-            [SpringBoot, Gradle, React, Vue2],
-            [MySQL, Redis, Kafka],
-            [Airflow],
-            [Docker, Kubernetes],
-            [Jenkins]
-        ],
-        color: 'primary.main',
-    },
-    {
-        company: "위대한상상(요기요)",
-        startDate: new Date('2025-03-04'),
-        current: true,
-        endDate: now,
-        skills: [
-            [Kotlin],
-            [SpringBoot, Gradle],
-            [Docker, Kubernetes],
-            [GithubActions]
-        ],
-        color: 'secondary.main'
-    }
+  {
+    company: 'NHN Cloud',
+    startDate: new Date('2022-01-03'),
+    endDate: new Date('2025-02-04'),
+    skills: [
+      [Java, TypeScript, Go],
+      [SpringBoot, Gradle, React, Vue2],
+      [MySQL, Redis, Kafka],
+      [Airflow],
+      [Docker, Kubernetes],
+      [Jenkins],
+    ],
+    color: 'primary.main',
+  },
+  {
+    company: '위대한상상(요기요)',
+    startDate: new Date('2025-03-04'),
+    current: true,
+    endDate: now,
+    skills: [
+      [Kotlin],
+      [SpringBoot, Gradle],
+      [Docker, Kubernetes],
+      [GithubActions],
+    ],
+    color: 'secondary.main',
+  },
 ];
 
 const WorkExperience = ({ id }) => {
-    return (
-        <Box 
-            id={id}
-            sx={{ 
-                flexGrow: 1,
-                bgcolor: 'rgba(76, 175, 80, 0.12)',
-                borderRadius: 4,
-                py: 4,
-                px: 2,
-            }}
-        >
-            <Typography variant={"h4"} sx={{ mb: 4, pl: 2 }}>경력</Typography>
-            
-            <Stack spacing={4}>
-                {baseData.map((exp) => (<WorkExperienceItem exp={exp} key={exp.company} />))}
-            </Stack>
-        </Box>
-    );
+  return (
+    <Box
+      id={id}
+      sx={{
+        flexGrow: 1,
+        bgcolor: 'rgba(76, 175, 80, 0.12)',
+        borderRadius: 4,
+        py: 4,
+        px: 2,
+      }}
+    >
+      <Typography variant={'h4'} sx={{ mb: 4, pl: 2 }}>
+        경력
+      </Typography>
+
+      <Stack spacing={4}>
+        {baseData.map((exp) => (
+          <WorkExperienceItem exp={exp} key={exp.company} />
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default WorkExperience;
