01f64d0483a1d0e65361cd658ac111c3aebdfe86
jk6841 <jk6841@gmail.com>
2025-03-14 19:39:03 +0900

    위대한상상 추가

---
+++ src/components/skills/Kotlin.jsx
@@ -0,0 +1,13 @@
+import BaseSkill from './BaseSkill';
+
+const Kotlin = () => {
+    return (
+        <BaseSkill 
+            name="Kotlin"
+            color="#F18E33"
+            docsUrl="https://kotlinlang.org/docs/home.html"
+        />
+    );
+};
+
+export default Kotlin; 
